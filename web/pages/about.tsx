20b136793c81a1546283c2ea4899672629717f61
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2020-02-10 06:42:01 -0800

    update about

--- web/pages/about.tsx
+++ web/pages/about.tsx
@@ -6,9 +6,10 @@ import MaxContainer from '../components/MaxContainer';
 const copy = {
   heading: `Auburn Computer Science Online Course Portal`,
   main: [
-    `This application is meant as an overview of the current curriculum for the <a class=" link--text " href="https://csonline.eng.auburn.edu/">Auburn Online Computer Science</a> program. It provides a method for students to rate their classes based on the quality, difficulty, and time spent.`,
+    `This application is meant as an overview of the current curriculum for the <a class=" link--text " href="https://csonline.eng.auburn.edu/">Auburn Online Computer Science</a> program. It provides a method for students to track their progress and rate their classes based on the quality, difficulty, and time spent.`,
     `If you are a student enrolled in the program, you can hit "Login" up top, enter your Auburn/Microsoft credentials (the same ones you use to access auaccess/Canvas), and rate the classes you've completed. Once you've entered a rating, the graph on the home page will update with your completed courses.`,
-    `This project was created by <a class=" link--text " href="//aboutevan.com">Evan Salzbrenner<a/>, a software engineer and student in the program. The code can be <a class=" link--text " href="https://github.com/salzbrenner/aucs-course-portal">viewed here</a>.`,
+    `This project was created by <a class=" link--text " href="//aboutevan.com">Evan Salzbrenner<a/>, a software developer and student in the program. You can <a class=" link--text " href="https://aboutevan.com/building-the-aucs-course-portal-with-python-and-javascript">read about its development</a> or <a class=" link--text " href="https://github.com/salzbrenner/aucs-course-portal">view the code</a>.`,
+    `This application is an independent student project and not supported by Auburn University or reflective of their views.`,
   ],
 };
 
